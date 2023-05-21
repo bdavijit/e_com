2bb650bb312ce73598a2b269cc31fba2894d534e
bdavijit <bdavijit@gmail.com>
2023-05-21 13:05:43 +0600

    router links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Notfound from './components/Notfound/Notfound';
+import About from './components/About/About';
+import Products from './components/Products/Products';
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+        <>
+              <Header />
+              <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/Products' element={<Products />} />
+                    <Route path='*' element={<Notfound />} />
+              </Routes>
+        </>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,18 +5,11 @@ const Header = () => {
     return (
           <header>
                 <div>title</div>
-                <menu>
-                      <Link className='' to='/'>Home</Link>
-                      <Link className='' to='/products'>
-                            products
-                      </Link>
-                      <Link className='' to='/AddNewReview'>
-                            Add A Review
-                      </Link>
-                      <Link className='' to='/Add'>
-                            Add A Review
-                      </Link>
-                </menu>
+                <nav>
+                      <Link to='/'>Home</Link>
+                      <Link to='/Products'>Products</Link>
+                      <Link to='/about'>About</Link>
+                </nav>
           </header>
     );
 };
