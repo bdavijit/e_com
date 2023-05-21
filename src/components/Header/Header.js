a58b0f3a543be8e64ce4587e7132dd5bc697585d
bdavijit <bdavijit@gmail.com>
2023-05-21 12:59:09 +0600

    router setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+          <header>
+                <div>title</div>
+                <menu>
+                      <Link className='' to='/'>Home</Link>
+                      <Link className='' to='/products'>
+                            products
+                      </Link>
+                      <Link className='' to='/AddNewReview'>
+                            Add A Review
+                      </Link>
+                      <Link className='' to='/Add'>
+                            Add A Review
+                      </Link>
+                </menu>
+          </header>
     );
 };
 
