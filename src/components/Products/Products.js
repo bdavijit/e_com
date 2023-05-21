2ccfe186161442127b407af339cbe557ee6dcb7c
bdavijit <bdavijit@gmail.com>
2023-05-21 14:19:07 +0600

    fatch data

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,7 +1,30 @@
-import React from 'react';
+
+import React, { useEffect, useState } from "react";
+
+
 
 const Products = () => {
-    return <div>Products</div>;
+    
+    const [products, setProducts] = useState([0, 5]);
+    console.log(products);
+    
+  useEffect(() => {
+        fetch('products.JSON').then(async (response) => {
+              try {
+                    const data = await response.json();
+                    // console.log("response data?", data);
+                    setProducts(data);
+              } catch (error) {
+                    console.log('Error happened here!');
+                    console.error(error);
+              }
+        });
+  }, []);
+   
+   
+   return <div>
+    Products
+    </div>;
 };
 
 export default Products;
